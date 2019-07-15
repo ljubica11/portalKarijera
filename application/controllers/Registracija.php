78d155a3899b0f8df3db420e5a8ac6be9e814ddf
ljubica11 <ljkrstic1@gmail.com>
2019-07-15 14:19:35 +0200

    Kontroler za registraciju
    Izvlacimo sve promenljive iz input polja i nakon toga pozivamo tri metode modela zaredom - prva ubacuje korisnika u tabelu "korisnik", druga dohvata id tog novog korisnika, treca sa tim id-jem ubacuje ostatak podataka u tabelu "student".

--- application/controllers/Registracija.php
+++ application/controllers/Registracija.php
@@ -6,6 +6,32 @@ class Registracija extends CI_Controller {
     
     public function index(){
         $this->load->view('registracija');
+ 
+    }
+    
+    public function reg(){
+        $korisnicko= $this->input->post('korisnicko');
+        $lozinka = $this->input->post('lozinka');
+        $email= $this->input->post('email');
+        $tip= "s";
+        
+        $ime= $this->input->post('ime');
+        $srednjeIme = $this->input->post('srednjeIme');
+        $prezime = $this->input->post('prezime');
+        $datum = $this->input->post('datum');
+        $pol = $this->input->post('gridRadios');
+        $drzavljanstvo = $this->input->post('drzavljanstvo');
+        $telefon = $this->input->post('telefon');
+        $adresa = $this->input->post('adresa');
+        $mesto = $this->input->post('mesto');
+        $pin = $this->input->post('pin');
+        $status = $this->input->post('status');
+        
+        $this->load->model('RegistrationModel');
+        $this->RegistrationModel->dodajKorisnika($korisnicko, $lozinka, $email, $tip);
+        $Korisnik= $this->RegistrationModel->dohvatiId($korisnicko);
+        $idKor = $Korisnik[0]['idKor'];
+        $this->RegistrationModel->dodajStudenta($ime, $srednjeIme, $prezime, $datum, $pol, $drzavljanstvo, $telefon, $adresa, $mesto, $pin, $status, $idKor);
     }
     
 }
