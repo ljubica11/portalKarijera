f7a93c4332f90b72b9ccbab81a201e852eea6897
ljubica11 <ljkrstic1@gmail.com>
2019-07-16 10:57:11 +0200

    View Template
    Pravimo template za ucitavanje headera, middle dela i footera u zavisnosti od toga koja je vrsta korisnika ulogovana.

---
+++ application/views/viewTemplate.php
@@ -0,0 +1,14 @@
+<?php
+if($this->session->user == null){
+    $this->load->view("headers/guestHeader");
+    $this->load->view("footers/guestFooter");
+} else if($this->session->userdata('user')['tip']=="s"){
+    $this->load->view("headers/studentHeader");
+    $this->load->view("footers/userFooter",["tip" => "student"]);
+} else if($this->session->userdata('user')['tip']== "k"){
+    $this->load->view("headers/kompHeader");
+    $this->load->view("footers/userFooter",["tip" => "kompanija"]);
+}
+
+$this->load->view($middle, $middle_data ?? []);
+
