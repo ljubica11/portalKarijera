ea44426012f9230dbd8444bd0b770d596a440049
ljubica11 <ljkrstic1@gmail.com>
2019-07-16 10:52:44 +0200

    Guest Header
    Header za korisnika koji nije ulogovan...

---
+++ application/views/headers/guestHeader.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/css/styleGuest.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title></title>
+    </head>
+    <body>
+        
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Portal Karijera</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Vesti<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Diskusije</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Registracija</a>
+                </li>
+              </ul>
+            </div> 
+        </nav>
+        
+        
