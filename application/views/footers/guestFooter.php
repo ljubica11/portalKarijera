255653b89082c971ab253cbfa495a94859d06a7b
ljubica11 <ljkrstic1@gmail.com>
2019-07-16 10:51:19 +0200

    Guest Footer
    Footer za korisnika koji nije ulogovan.

---
+++ application/views/footers/guestFooter.php
@@ -0,0 +1,9 @@
+
+        <div class="footer">
+            <a href="#">O nama</a>
+            <a href="#">Kontakt</a>
+            <a href="#">Uslovi korišćenja</a>
+            <span class="copy"> copyright Karijera.rs </span>
+        </div>
+    </body>
+</html>
