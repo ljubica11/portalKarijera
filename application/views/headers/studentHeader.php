5dc1a95e715527453c0347e8b5b0888d8abef82e
gordst <33371225+gordst@users.noreply.github.com>
2019-07-19 13:36:43 +0200

    klik na homepage je user/index privremeno

--- application/views/headers/studentHeader.php
+++ application/views/headers/studentHeader.php
@@ -10,7 +10,7 @@
     <body>
         
         <nav class="navbar navbar-expand-lg navbar-dark">
-            <a class="navbar-brand" href="#">Portal Karijera</a>
+            <a class="navbar-brand" href="<?php echo site_url("User/index")?>">Portal Karijera</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
