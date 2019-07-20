c76f038a3d7852ab30de9267876309dfd989f013
ljubica11 <ljkrstic1@gmail.com>
2019-07-20 15:19:15 +0200

    Dodavanje slike
    Izmene u User Controlleru (dodajemo metode za ubacivanje slike, ubacivanje CV-a i citanje CV-a). Takodje smo dodali i metode za dohvatanje podataka za kompaniju, pa se sada u zavisnosti od toga da li je user sa tipom "s" ili "k" prosledjuju razliciti podaci na view.

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -11,7 +11,7 @@ class User extends CI_Controller{
         
         if($tip == 's'){
             $data = [];
-            $data["middle_data"] = ["podaci" => $this->UserModel->podaciZaStudenta($id),
+            $data["middle_data"] = ["podaciStudent" => $this->UserModel->podaciZaStudenta($id),
                                     "interesovanja" => $this->UserModel->imaInteresovanja($id),
                                     "vestine" => $this->UserModel->imaVestine($id),
                                     "diploma" => $this->UserModel->imaDiplomu($id),
@@ -21,7 +21,9 @@ class User extends CI_Controller{
            
             
         }else if($tip == 'k'){
-            $data['middle_data']= $this->UserModel->podaciZaKompaniju($id);
+            $data['middle_data']= ["podaciKompanija" => $this->UserModel->podaciZaKompaniju($id),
+                                   "oglasi" => $this->UserModel->imaOglase($id),
+                                   "obavestenja" => $this->UserModel->imaObavestenja($id)];
             $data["middle"] = "middle/pocetna";
             $this->load->view('viewTemplate', $data);
             
@@ -32,5 +34,78 @@ class User extends CI_Controller{
           $this->session->sess_destroy();
           redirect("Login");
       }
+      
+      public function novaSlika(){
+          
+          $idKor= $this->session->userdata('user')['idKor'];
+                $config['allowed_types'] = 'gif|jpg|png|jpeg';
+                $config['max_size'] = 2048;
+                $config['max_width'] = 2048;
+                $config['max_height'] = 1536;
+          //$config= $this->config->item('upload');
+             if(!is_dir('./userImg/'.$idKor)){
+                mkdir('./userImg/'.$idKor, 0777);
+        }
+          $config['upload_path']= './userImg/'.$idKor;
+        
+          $this->load->library('upload', $config);
+          if (!$this->upload->do_upload('image')){
+            echo $this->upload->display_errors();
+        }else{
+            $this->index();
+      }
     
+    }
+    
+        public function dodajCV(){
+            $idKor= $this->session->userdata('user')['idKor'];
+            $config['allowed_types'] = 'pdf';
+            $config['max_size'] = '1000';
+            
+            if(!is_dir('./CV/'.$idKor)){
+                mkdir('./CV/'.$idKor, 0777);
+        }
+          $config['upload_path']= './CV/'.$idKor;
+          $this->load->library('upload', $config);
+          if(!$this->upload->do_upload('cv')){
+              $this->index();
+              echo $this->upload->display_errors();
+          }else{
+              $upload_data = $this->upload->data();
+              echo $upload_data["file_type"];
+               $this->index();
+          }
+
+        }
+        
+//        public function dohvatiCV(){
+//            $idKor= $this->session->userdata('user')['idKor'];
+//            $dir = './CV/'.$idKor;
+//            $this->load->helper('download');
+//            $allCV= scandir($dir);
+//                $onlyCV = array_diff($allCV, array('.', '..'));
+//                foreach($onlyCV as $oneCV){
+//                    force_download($dir.'/'.$oneCV, null);
+//                }
+//            
+//        }
+        
+        public function procitajCV(){
+            $idKor= $this->session->userdata('user')['idKor'];
+            $dir = './CV/'.$idKor;
+            $allCV= scandir($dir);
+                $onlyCV = array_diff($allCV, array('.', '..'));
+                foreach($onlyCV as $oneCV){
+                    $file = $dir.'/'.$oneCV;
+                    $filename= $oneCV;
+                    header('Content-type: application/pdf');
+                    header('Content-Disposition: inline; filename="' . $filename . '"');
+                    header('Content-Transfer-Encoding: binary');
+                    header('Content-Length: ' . filesize($file));
+                    header('Accept-Ranges: bytes');
+                    readfile($file);
+                    
+                }
+        }
+        
 }
