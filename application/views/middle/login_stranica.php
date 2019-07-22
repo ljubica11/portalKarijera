194a3c7ece6c526c54e96991afcfe82991f46417
SanjaKordic <kordic.l.sanja@gmail.com>
2019-07-22 12:07:15 +0200

    Naslovna-middle
    Ubacena je slika za pozadinu i natpis, kao i dugme registracija, zatim uvezene su slike sponzora koji pruzaju podrsku projektu.

--- application/views/middle/login_stranica.php
+++ application/views/middle/login_stranica.php
@@ -1,4 +1,58 @@
-
-
-   ovde treba da bude neki sadrzaj
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <meta name="viewport" 
+        content="width=device-width, initial-scale=1">
+        <title></title>
+    </head>
+    <body>
+        <div class="container" id="container1">
+            <div class="row">
+                <div class="offset-sm-12" style="height: 50px"></div>
+            </div>
+            <div class="row">
+                <div class="offset-sm-1 col-sm-3 text-white">
+                    <h1>Povezivanje studenata i</h1>
+                </div>
+                <div class="offset-sm-8"></div>
+            </div>
+            <div class="row">
+                <div class="offset-sm-1 col-sm-3 text-white">
+                    <h1>kompanija radi ubrzanja inovacija</h1>
+                </div>
+                <div class="offset-sm-8"></div>
+            </div>
+            <div class="row">
+                <div class="offset-sm-12" style="height: 50px"></div>
+            </div>
+            <div class="row">
+                <div class="offset-sm-1 col-sm-3 btn btn-outline-success btn-lg" type="submit">
+                    Registracija
+                </div>
+                <div class="offset-sm-8"></div>
+            </div>
+            <div class="row">
+                <div class="offset-sm-12" style="height: 250px"></div>
+            </div>
+        </div>
+        <div class="container" id="container2">
+            <div class="row">
+                <div class="offset-sm-12" style="height: 20px"></div>
+            </div>
+            <div class="row" style="align-top">
+                <div class="col-sm-2"><img src="<?php echo base_url('/images/firma1.png')?>" class="img-fluid" alt="Responsive image"></div>
+                <div class="col-sm-1"><img src="<?php echo base_url('/images/firma2.png')?>" class="img-fluid" alt="Responsive image"></div>
+                <div class="col-sm-1"><h1>ETF</h1></div>
+                <div class="col-sm-2"><img src="<?php echo base_url('/images/firma3.png')?>" class="img-fluid" alt="Responsive image"></div>
+                <div class="col-sm-2"><img src="<?php echo base_url('/images/firma4.png')?>" class="img-fluid" alt="Responsive image"></div>
+                <div class="col-sm-2"><img src="<?php echo base_url('/images/firma5.png')?>" class="img-fluid" alt="Responsive image"></div>
+                <div class="col-sm-2"><img src="<?php echo base_url('/images/firma6.png')?>" class="img-fluid" alt="Responsive image"></div>
+            </div>
+        </div>
+        <div class="container" id="container3">
+            
+        </div>
+    </body>
+</html>
    
