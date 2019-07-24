c8c849a6cdc6a617ffb43b86af049512e41cf922
SanjaKordic <kordic.l.sanja@gmail.com>
2019-07-24 19:18:11 +0200

    Link-Uslovi koriscenja
    Ubacen je link u footeru da prebaci na stranicu uslovi_koriscenja, ali ne radi, ne znam zasto, ako neko moze neka pogleda

--- application/views/footers/guestFooter.php
+++ application/views/footers/guestFooter.php
@@ -2,7 +2,7 @@
         <div class="footer">
             <a href="#">O nama</a>
             <a href="#">Kontakt</a>
-            <a href="#">Uslovi korišćenja</a>
+            <a href="<?php echo site_url('uslovi_koriscenja') ?>">Uslovi korišćenja</a>
             <span class="copy"> copyright Karijera.rs </span>
         </div>
     </body>
