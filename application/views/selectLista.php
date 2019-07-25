ae5ba547fd2e1832e7203f95da9a343106ba4694
ljubica11 <ljkrstic1@gmail.com>
2019-07-25 17:05:58 +0200

    View za ispisivanje select lista
    Odvojeni view fajl preko kojeg se ispisuju rezultati ajax f-je za dodavanje nove opcije u sifrarnik. Ovo ispisuje nove padajuce select liste sa novim podacima, ukljucuci i najnoviji dodatak (koji je korisnik uneo). Novi dodatak je uvek selektovan.

---
+++ application/views/selectLista.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<?php if(isset($tip) and $tip == "mesto"){ ?>
+<select name="mesto" onchange="dodajInput('mesto')" id="listamesto"> 
+            <option disabled value="">Mesto</option>
+              <?php 
+            foreach ($mesta as $mesto){
+                $naziv = $mesto["naziv"];
+                $idG = $mesto["idGra"];
+                if($naziv == $novo){
+                    echo "<option value='$idG' selected>$naziv</option>";
+                }else{
+                    echo "<option value='$idG'>$naziv</option>"; 
+                }
+            }
+            ?>
+           <option class="dodaj" value="dodaj">Dodaj novo mesto</option>
+</select>
+<?php } else if(isset($tip) and $tip == "sediste"){ ?>
+<select name="sediste" id="listasediste" onchange="dodajInput('sediste')">
+    <option disabled value="">Sediste</option>
+    <?php 
+           foreach ($mesta as $mesto){
+                $naziv = $mesto["naziv"];
+                $idG = $mesto["idGra"];
+                if($naziv == $novo){
+                    echo "<option value='$idG' selected>$naziv</option>";
+                }else{
+                    echo "<option value='$idG'>$naziv</option>"; 
+                }
+            }
+            ?>
+    <option value="dodaj">Dodaj grad</option>
+</select>
+            
+    
+<?php } else if(isset($fakulteti)){ ?>
+<select name="fakultet" id="listafakultet" onchange="dodajInput('fakultet')">
+    <option disabled value="">Odaberite fakultet</option>
+    <?php 
+            foreach ($fakulteti as $fakultet){
+                $naziv = $fakultet["naziv"];
+                $idFak = $fakultet["idFak"];
+                if($naziv == $novo){
+                  echo "<option value='$idFak' selected>$naziv</option>";  
+                }else{
+                  echo "<option value='$idFak'>$naziv</option>";
+               }
+            }
+            ?>
+    <option value="dodaj">Dodaj fakultet</option>
+</select>
+<?php } else if(isset($kompanije)){ ?>
+<select name="kompanija" id="listakompanija" onchange="dodajInput('kompanija')">
+    <option disabled value="">Odaberite kompaniju</option>
+    <?php 
+            foreach ($kompanije as $kompanija){
+                $naziv = $kompanija["naziv"];
+                $idKom = $kompanija["idSifKo"];
+                if($naziv == $novo){
+                  echo "<option value='$idKom' selected>$naziv</option>";
+                }else{
+                  echo "<option value='$idKom'>$naziv</option>";
+                   }
+                }
+                ?>
+    <option value="dodaj">Dodaj kompaniju</option>
+</select>
+
+<?php } else if(isset ($pozicije)){ ?>
+<select name="pozicija" id="listapozicija" onchange="dodajInput('pozicija')">
+    <option disabled value="">Odaberite poziciju</option>
+                    <?php 
+                            foreach ($pozicije as $pozicija){
+                                $naziv = $pozicija["naziv"];
+                                $idPoz = $pozicija["idPoz"];
+                                if($naziv == $novo){
+                                    echo "<option value='$idPoz' selected>$naziv</option>";
+                                }else{
+                                echo "<option value='$idPoz'>$naziv</option>";
+                                }
+                            }
+                            ?>
+    <option value="dodaj">Dodaj poziciju</option>
+</select>
+<?php } ?>
+
+
+
+
