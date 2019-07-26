4c6dc0330d7ef31b84ea8c306e89ffeca0d73ae1
ljubica11 <ljkrstic1@gmail.com>
2019-07-26 16:05:28 +0200

    View za oglase.
    Ovo je glavni view, u kojem se bukvalno samo echo-uju promenljive, a unutar promenljive se nalazi citav view (levo za pretragu, u sredini za ispis oglasa).

---
+++ application/views/middle/oglasi.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-3 levo"> 
+          <?php echo $pretraga; ?>
+        </div>
+        
+        <div class="col-6">  
+            <?php echo $oglasi; ?>
+        </div>
+        
+        <div class="col-3">
+        </div>
+            
+        </div>
+    </div>
+
+
+
