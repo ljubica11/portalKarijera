9823b62a058ae58acc96be0cea79eaa8b7cbec39
sasadjekic <djeka78@com.com>
2019-07-28 13:14:14 +0200

    Vesti
    4 nova fajla i dve izmene tj dodavanje
    linkova u studentHeader i kompHeader

--- application/views/headers/kompHeader.php
+++ application/views/headers/kompHeader.php
@@ -3,7 +3,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/css/styleKompanija.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/css/styleKompanija.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -11,32 +11,32 @@
         <title></title>
     </head>
     <body>
-        
+
         <nav class="navbar navbar-expand-lg navbar-dark">
             <a class="navbar-brand" href="#"><img src="images/logo.png" width="90" height="60" title="Logo" alt="Logo" />Portal Karijera</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Vesti</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Obaveštenja</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="<?php echo site_url("Diskusije/index")?>">Diskusije</a>
-                </li> 
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Oglasi</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Ankete</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Pretraga sajta</a>
-                </li>
-              </ul>
-            </div> 
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo site_url("Vesti/index") ?>">Vesti</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Obaveštenja</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo site_url("Diskusije/index") ?>">Diskusije</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Oglasi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Ankete</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pretraga sajta</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
