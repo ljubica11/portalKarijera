0909ec537aee52728f411d5eae212397e18c3ba7
ljubica11 <ljkrstic1@gmail.com>
2019-08-05 16:12:44 +0200

    Izmene u hederu
    Klikom na "portal Karijera" se vracamo na profil, tj na pocetnu stranu

--- application/views/headers/guestHeader.php
+++ application/views/headers/guestHeader.php
@@ -13,7 +13,7 @@
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#"><img id="brand-image" src="../images/logo.png" width="90" height="60" title="Logo" alt="Logo" />Portal Karijera</a>
+            <a class="navbar-brand" href="<?php echo site_url('Login')?>"><img id="brand-image" src="../images/logo.png" width="90" height="60" title="Logo" alt="Logo" />Portal Karijera</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
