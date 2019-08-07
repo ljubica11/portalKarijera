f3869bcfa42d062667cc80637ee0346e542f3196
SanjaKordic <kordic.l.sanja@gmail.com>
2019-08-07 13:30:19 +0200

    footer-dodati linkovi

--- application/views/footers/guestFooter.php
+++ application/views/footers/guestFooter.php
@@ -1,9 +1,9 @@
 
         <div class="footer">
-            <a href="#">O nama</a>
+            <a href="http://localhost/portalKarijera/index.php/Login/oNama">O nama</a>
             <a href="#">Kontakt</a>
             <a href="http://localhost/portalKarijera/index.php/Login/usloviKoriscenja">Uslovi korišćenja</a>
-            <span class="copy"> copyright Karijera.rs </span>
+            <span class="copy" style="color: white"> copyright Karijera.rs </span>
         </div>
     </body>
 </html>
