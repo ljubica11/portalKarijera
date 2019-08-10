a32c4ed7c92bd01cecc299dd24d62b69ea2c3651
gordst <33371225+gordst@users.noreply.github.com>
2019-08-10 13:17:54 +0200

    promene u funkcionalnosti Diskusije
    1. Klikom na "arhiviraj" nestaje btn "dodaj post", tako da se kod arhiviranih diskusija postovi mogu samo pregledati. 2. post se prikazuje odmah po dodavanju u diskusiji.

--- application/views/diskusije/disk.php
+++ application/views/diskusije/disk.php
@@ -15,7 +15,7 @@ if (isset($diskusije)) {
         <b>Datum pokretanja: </b><?php echo $d['datum'] ?><br/> 
         <?php $id = $d['idDis'] ?>
         <?php echo "<a href='#' class='badge badge-primary' onclick ='postovi($id)'> <b>Pogledaj postove</b></a>" ?>
-        <?php echo "<a href='#' class='badge badge-primary' onclick ='dodajdiv($id)'> <b>Dodaj post</b></a>" ?>
+        <?php if($d['vidljivost'] != 'autor'){ echo "<a href='#' class='badge badge-primary' onclick ='dodajdiv($id)'> <b>Dodaj post</b></a>" ;} ?>
         <?php if($this->session->userdata('user')['korisnicko']== $autor && $d['vidljivost'] != 'autor'){ echo "<a href='#' class='badge badge-primary float-right' onclick ='arhiviraj($id)'> <b>Arhiviraj</b></a><br/>" ;}?>
 
 
