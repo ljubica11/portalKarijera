46ffe07ef21c0ee489c21ae0a897ab718599d82b
SanjaKordic <kordic.l.sanja@gmail.com>
2019-08-11 12:43:06 +0200

    GuestModel-najnovije
    Dohvatanje iz baze najnovijih vesti, obav., diskusije i oglasa

--- application/models/GuestModel.php
+++ application/models/GuestModel.php
@@ -16,17 +16,43 @@
              $this->load->view('Login/oNama');
         }
         
-        public function najnovijaVest($idVes) {
-            $this->db->select('max(idVes) as idVes');
-            $this->db->SUBSTRING_INDEX('(GROUP_CONCAT(`datum` ORDER BY `datum` DESC), ',', 1) AS datum');
-            $this->db->SUBSTRING_INDEX('(GROUP_CONCAT(`naziv` ORDER BY `datum` DESC), ',', 1) AS naziv');
-            $this->db->SUBSTRING_INDEX('(GROUP_CONCAT(`tekst` ORDER BY `datum` DESC), ',', 1) AS tekst');
-            $this->db->SUBSTRING_INDEX('(GROUP_CONCAT(`autor` ORDER BY `datum` DESC), ',', 1) AS autor');
-            $this->db->FROM ('vesti');
-            $query = $this->db->get();
-        return $query -> result_array();
-        var_dump($query);
-
+        public function najnovijaVest() {
+            $this->db->select('tekst');
+            $this->db->from('vesti');
+            $this->db->order_by('datum', 'DESC');
+            $this->db->limit(1);
+            $query=$this->db->get();
+            return $query->result_array();   
+            }
+            
+         public function najnovijeObavestenje() {
+            $this->db->select('tekst');
+            $this->db->from('obavestenja');
+            $this->db->order_by('datum', 'DESC');
+            $this->db->limit(1);
+            $query=$this->db->get();
+            return $query->result_array();           
+            }
+       
+        public function najnovijiOglas() {
+            $this->db->select('opis');
+            $this->db->from('oglasi');
+            $this->db->order_by('vremePostavljanja', 'DESC');
+            $this->db->limit(1);
+            $query=$this->db->get();
+            return $query->result_array();   
+        }
+        
+        public function najnovijaDiskusija() {
+            $this->db->select('opis');
+            $this->db->from('diskusija');
+            $this->db->order_by('datum', 'DESC');
+            $this->db->limit(1);
+            $query=$this->db->get();
+            return $query->result_array();
+            
         }
     }
-?>
+        
+        
+    
