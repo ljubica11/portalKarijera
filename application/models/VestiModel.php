27b34114053efc5a9305e38a8b2e226a267a8498
sasadjekic <djeka78@com.com>
2019-08-13 23:50:46 +0200

    Ispis svih vesti pored kategorija vesti nakon klika na link u header-u.

--- application/models/VestiModel.php
+++ application/models/VestiModel.php
@@ -77,4 +77,14 @@ class VestiModel extends CI_Model {
         $this->db->insert('sifkategorijavesti');
     }
 
+    public function dohvatiSveVesti() {
+
+        $this->db->select('vesti.*, korisnik.korisnicko as korisnik');
+        $this->db->from('vesti');
+        $this->db->join('korisnik', 'korisnik.idKor = vesti.autor');
+        $this->db->order_by('idVes', 'DESC');
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+
 }
