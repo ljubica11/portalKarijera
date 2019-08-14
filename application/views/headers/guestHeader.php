ff05b4746c98dd93f798224cfc7b506d005ecb7c
SanjaKordic <kordic.l.sanja@gmail.com>
2019-08-14 18:01:05 +0200

    Update guestHeader.php

--- application/views/headers/guestHeader.php
+++ application/views/headers/guestHeader.php
@@ -10,7 +10,7 @@
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-light" id="header">
-            <a class="navbar-brand" href="#"><img id="brand-image" src="http://localhost/portalKarijera//images/logo.png" width="90" height="60" title="Logo" alt="Logo">PortalKarijera</a>
+            <a class="navbar-brand" href="#"><img id="brand-image" src="<?php echo base_url()?>//images/logo.png" width="90" height="60" title="Logo" alt="Logo">PortalKarijera</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
