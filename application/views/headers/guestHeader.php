ab4fc0b43999a9ebceb9aff3efe78d67e68c43b8
SanjaKordic <kordic.l.sanja@gmail.com>
2019-08-19 12:04:25 +0200

    dodata boja pozadine u body

--- application/views/headers/guestHeader.php
+++ application/views/headers/guestHeader.php
@@ -6,15 +6,14 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
          <script src="https://kit.fontawesome.com/2dd4f4a4ca.js"></script><link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free.min.css" media="all"><link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free-v4-font-face.min.css" media="all"><link rel="stylesheet" href="https://kit-free.fontawesome.com/releases/latest/css/free-v4-shims.min.css" media="all">
-        <title></title>
+         <title></title>
     </head>
-    <body>
+    <body style="background-color: #e6e6e6">
         <nav class="navbar navbar-expand-lg navbar-light" id="header">
             <a class="navbar-brand" href="<?php echo site_url('Login')?>"><img id="brand-image" src="<?php echo base_url()?>//images/logo.png" width="90" height="60" title="Logo" alt="Logo">PortalKarijera</a>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-
-              <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
               <ul class="navbar-nav ml-auto">
@@ -54,8 +53,26 @@
                 $(".fa-search").click(function(){
                   $(".wrap, .input").toggleClass("active");
                   $("input[type='text']").focus();
+                  $route[search-going-data]='Search/search_going_data';
+                  $('#goingto').on('keyup',function(){
+                      var gointval=$('#gointTo').val();
+                      if(gointval==""){
+                          alert("no data");
+                      }else{
+                          $.ajax({
+                              url:"<?php echo base_url('search-going-data'); ?>",
+                              type:'POST',
+                              data:{searchval:gointval},
+                              success:function(res){
+                                  console.log(res);
+                              }
+                          });
+                      }
+                  });
                 });
           });
+           
+          
     
         </script>
 
