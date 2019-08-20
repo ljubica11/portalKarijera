e68e0d6cc4f064373954875c74f38279db79b563
gordst <33371225+gordst@users.noreply.github.com>
2019-08-20 12:13:32 +0200

    nove funkcionaklnosti - admin salje mejlom statisticke izvestaje
    Admin salje mejlom izvestaje generisane u pdf formatu. Bira liste primalaca - kompanije ili administratore.

--- application/controllers/Statistika.php
+++ application/controllers/Statistika.php
@@ -6,38 +6,123 @@
  * @author gordan
  */
 class Statistika extends CI_Controller {
-    
-    public function __construct(){
+
+    public function __construct() {
         parent::__construct();
-        
-      
+
+
         $this->load->model('StatistikaModel');
         $this->load->model('SifrarniciModel');
+        $this->load->library('pdf');
     }
-    
-    public function index(){
-        
+
+    public function index() {
+
         $studenti = $this->db->count_all_results('student');
         $zaposleniStudenti = $this->StatistikaModel->zaposleniStudenti();
-        $nezaposleniStudenti = $this->StatistikaModel->nezaposleniStudenti(); 
+        $nezaposleniStudenti = $this->StatistikaModel->nezaposleniStudenti();
         $phpkurs = $this->StatistikaModel->phpkurs();
         $javakurs = $this->StatistikaModel->javakurs();
         $linuxkurs = $this->StatistikaModel->linuxkurs();
         $fakulteti = $this->SifrarniciModel->dohvatiFakultete();
-        foreach($fakulteti as $f){
+        $mejlLista = $this->StatistikaModel->mejlLista();
+        $timKarijera = $this->StatistikaModel->mejlAdmini();
+        foreach ($fakulteti as $f) {
             $idFak = $f['idFak'];
-            }
+        }
         $diploma = $this->StatistikaModel->diploma($idFak);
         $zbirDiploma = $this->db->count_all_results('diploma');
-        
-        
-       
-    
+
+
+
+
         $data['middle_data'] = ['studenti' => $studenti, 'zaposleni' => $zaposleniStudenti, 'nezaposleni' => $nezaposleniStudenti,
-                                'phpkurs' => $phpkurs, 'javakurs' => $javakurs, 'linuxkurs' => $linuxkurs, 'diploma' => $diploma,
-                                'fakulteti' => $fakulteti, 'zbirDiploma' => $zbirDiploma];
+            'phpkurs' => $phpkurs, 'javakurs' => $javakurs, 'linuxkurs' => $linuxkurs, 'diploma' => $diploma,
+            'fakulteti' => $fakulteti, 'zbirDiploma' => $zbirDiploma, 'timKarijera' => $timKarijera,
+            'mejlLista' => $mejlLista];
         $data['middle'] = 'middle/stats';
         $this->load->view('viewTemplate', $data);
     }
+
     
+    public function saljiIzvestaj() {
+
+
+        $studenti = $this->db->count_all_results('student');
+        $zaposleniStudenti = $this->StatistikaModel->zaposleniStudenti();
+        $nezaposleniStudenti = $this->StatistikaModel->nezaposleniStudenti();
+        $phpkurs = $this->StatistikaModel->phpkurs();
+        $javakurs = $this->StatistikaModel->javakurs();
+        $linuxkurs = $this->StatistikaModel->linuxkurs();
+        $fakulteti = $this->SifrarniciModel->dohvatiFakultete();
+        foreach ($fakulteti as $f) {
+            $idFak = $f['idFak'];
+        }
+        $diploma = $this->StatistikaModel->diploma($idFak);
+        $zbirDiploma = $this->db->count_all_results('diploma');
+        $data['middle_data'] = ['studenti' => $studenti, 'zaposleni' => $zaposleniStudenti, 'nezaposleni' => $nezaposleniStudenti,
+            'phpkurs' => $phpkurs, 'javakurs' => $javakurs, 'linuxkurs' => $linuxkurs, 'diploma' => $diploma,
+            'fakulteti' => $fakulteti, 'zbirDiploma' => $zbirDiploma];
+        $data['middle'] = 'middle/stats';
+        $this->load->view('viewTemplate', $data);
+
+        $html = $this->load->view('middle/stats', $data, true);
+
+
+        //generisem pdf fajl
+
+        $this->load->dompdf->load_html($html);
+        $this->dompdf->render();
+        $pdf_filename = time() . '_' . 'stats' . '.pdf';
+        $pdf_string = $this->dompdf->output();
+        
+         //slanje pdf fajla mejlom
+        $this->load->library('Phpmailerlib');
+        $Mail = $this->phpmailerlib->load();
+        $mejlLista = $this->StatistikaModel->mejlLista();
+        $timKarijera = $this->StatistikaModel->mejlAdmini();
+        
+
+        $msg = 'Postovana/i, u prilogu izvestaj o strukturi studenata. '
+                . ' Srdacan pozdrav. ' . 'Portal Karijera tim';
+
+        $Mail->SMTPDebug = 2;
+        $Mail->Mailer = 'smtp';
+        $Mail->isSMTP();
+        $Mail->Host = "smtp.gmail.com";
+        $Mail->Port = 587;
+        $Mail->SMTPSecure = "tls";
+        $Mail->SMTPAuth = true;
+        $Mail->Username = "karijera.online@gmail.com";
+        $Mail->Password = "A123A123*";
+        $Mail->SetFrom("admin-karijera.online@gmail.com");
+        $Mail->Subject = 'Statistika';
+        $Mail->Body = $msg;
+        if ($this->input->get('listeMejlova') == 1) {
+            foreach ($mejlLista as $m) {
+                $mejl = $m['email'];
+                $Mail->AddAddress($mejl);
+            }
+        } else if ($this->input->get('listeMejlova') == 2) {
+
+            foreach ($timKarijera as $m) {
+                $mejl = $m['email'];
+                $Mail->AddAddress($mejl);
+            }
+        }
+
+        // $Mail->AddAddress("gordanst@gmail.com");
+        $Mail->addStringAttachment($pdf_string, $pdf_filename);
+
+    
+
+
+        if ($Mail->Send(true)) {
+            echo "Poruka poslata";
+        } else {
+            echo "Poruka nije poslata<br/>";
+            echo "GRESKA: " . $Mail->ErrorInfo;
+        }
+    }
+
 }
