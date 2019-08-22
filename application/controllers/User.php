ae26eee979a58c243331f654767e76d3a357e1dc
ljubica11 <ljkrstic1@gmail.com>
2019-08-22 12:05:54 +0200

    Korisnik menja podatke

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -28,38 +28,45 @@ class User extends CI_Controller{
         $tip= $this->session->userdata('user')['tip'];
         }
         if($tip == 's'){
-            $data = [];
-            $data["middle_data"] = ["podaciStudent" => $this->UserModel->podaciZaStudenta($id),
-                                    "interesovanja" => $this->UserModel->imaInteresovanja($id),
-                                    "vestine" => $this->UserModel->imaVestine($id),
-                                    "diploma" => $this->UserModel->imaDiplomu($id),
-                                    "iskustvo" => $this->UserModel->radnoIskustvo($id),
-                                    "idKor" => $id,
-                                    "tip" => $tip];
+            $dataLevo = ["podaciStudent" => $this->UserModel->podaciZaStudenta($id),
+                        "idKor" => $id,
+                        "tip" => $tip ];
+            
+            $dataCentar = ["interesovanja" => $this->UserModel->imaInteresovanja($id),
+                           "vestine" => $this->UserModel->imaVestine($id),
+                           "diploma" => $this->UserModel->imaDiplomu($id),
+                           "iskustvo" => $this->UserModel->radnoIskustvo($id),
+                           "idKor" => $id,
+                           "tip" => $tip];
+            
+            
+            $data["middle_data"] = ["osnovniPodaci" => $this->load->view('pocetna/pocetnaLevo', $dataLevo, true),
+                                    "dodatniPodaci" => $this->load->view('pocetna/pocetnaCentar', $dataCentar, true)];
+            
             $data["middle"] = "middle/pocetna";
+            
             $this->load->view('viewTemplate', $data);
            
             
         }else if($tip == 'k'){
-            $data['middle_data']= ["podaciKompanija" => $this->UserModel->podaciZaKompaniju($id),
-                                   "oglasi" => $this->OglasiModel->dohvatiOglaseKorisnika($id),
-                                   "obavestenja" => $this->UserModel->imaObavestenja($id),
-                                   "idKor" => $id,
-                                   "tip" => $tip];
+            
+            $dataLevo = ["podaciKompanija" => $this->UserModel->podaciZaKompaniju($id),
+                        "idKor" => $id,
+                        "tip" => $tip];
+            
+            $dataCentar = ["oglasi" => $this->OglasiModel->dohvatiOglaseKorisnika($id),
+                           "obavestenja" => $this->UserModel->imaObavestenja($id),
+                           "idKor" => $id,
+                           "tip" => $tip];
+            
+            
+            $data['middle_data']= ["osnovniPodaci" => $this->load->view('pocetna/pocetnaLevo', $dataLevo, true),
+                                   "dodatniPodaci" => $this->load->view('pocetna/pocetnaCentar', $dataCentar, true)];
+            
             $data["middle"] = "middle/pocetna";
             $this->load->view('viewTemplate', $data);
             
-        }else if($tip == 'a'){
-//           $sifrarnici = ["mesta" => $this->SifrarniciModel->dohvatiMesto(),
-//                          "drzavljanstva" => $this->SifrarniciModel->dohvatiDrz(),
-//                           "fakulteti" => $this->SifrarniciModel->dohvatiFakultete(),
-//                           "kompanije" => $this->SifrarniciModel->dohvatiKompanije(),
-//                           "pozicije" => $this->SifrarniciModel->dohvatiPozicije(),
-//                           "interesovanja" => $this->SifrarniciModel->dohvatiInteresovanja(),
-//                           "vestine" => $this->SifrarniciModel->dohvatiVestine(),
-//                           "katvesti" => $this->SifrarniciModel->dohvatiKategorijeVesti(),
-//                           "katdiskusija" => $this->SifrarniciModel->dohvatiKategorijeDiskusija()];
-//            $data["middle_data"] = ["izmenaSifrarnika" => $this->load->view('admin/prikazSifrarnika'), $sifrarnici, true];   
+        }else if($tip == 'a'){  
             $data["middle"] = "middle/adminPocetna";
             $this->load->view('viewTemplate', $data);
         }
@@ -155,4 +162,130 @@ class User extends CI_Controller{
             $this->load->view('pocetna/newsFeed', $data);
         }
         
+        public function formaIzmenaPodataka($id){
+            $this->load->model('UserModel');
+            $this->load->model('SifrarniciModel');
+            $data["middle_data"] = ["podaci" => $this->UserModel->podaciZaStudenta($id),
+                                    "kursevi" => $this->SifrarniciModel->dohvatiKurs(), 
+                                    "drzavljanstvo" => $this->SifrarniciModel->dohvatiDrz(),
+                                     "mesta" => $this->SifrarniciModel->dohvatiMesto()];
+            $data["middle"] = "middle/izmenaPodataka";
+            
+            $this->load->view('viewTemplate', $data);
+        }
+        
+        public function izmeniPodatke(){
+            $this->load->model('UserModel');
+
+            $this->form_validation->set_message('required', '{field} je obavezno polje');
+            $this->form_validation->set_message('valid_email', 'E-mail adresa nije u ispravnom formatu');
+            $this->form_validation->set_message('is_unique', 'Polje {field} mora biti jedinstveno.');
+            $this->form_validation->set_message('regex_match', '{field} nije odgovarajuceg formata. ');
+            $this->form_validation->set_message('matches', ' {field} i lozinka se ne poklapaju');
+            $this->form_validation->set_message('exact_length', '{field} mora da sadrzi tacno {param} cifre');  
+            
+            if($this->input->post("korisnicko") == $this->input->post("originalKorisnicko")){
+              $this->form_validation->set_rules('korisnicko', 'Korisnicko ime', 'required|min_length[3]');  
+            }else{
+            $this->form_validation->set_rules('korisnicko', 'Korisnicko ime', 'required|is_unique[korisnik.korisnicko]|min_length[3]');
+            }
+            if($this->input->post('email') == $this->input->post('originalEmail')){
+               $this->form_validation->set_rules('email', 'E-mail', 'required|valid_email'); 
+            }else{
+            $this->form_validation->set_rules('email', 'E-mail', 'required|valid_email|is_unique[korisnik.email]');
+            }
+            $this->form_validation->set_rules('ime', 'Ime', 'required');
+            $this->form_validation->set_rules('prezime', 'Prezime', 'required');
+            $this->form_validation->set_rules('datum', 'Datum rodjenja', 'required|callback_validateAge[18]');
+            $this->form_validation->set_rules('drzavljanstvo', 'Državljanstvo', 'required|numeric');
+            $this->form_validation->set_rules('kurs', 'Kurs', 'required|numeric');
+            $this->form_validation->set_rules('telefon', 'Telefon', 'required|regex_match[/^\d{3}\/?\d{6,7}$/]');
+            $this->form_validation->set_rules('adresa', 'Adresa', 'required');
+            $this->form_validation->set_rules('mesto', 'Mesto', 'required|alpha_numeric');
+            $this->form_validation->set_rules('status', 'Status', 'required|alpha');
+            
+            if ($this->form_validation->run() == 0) {
+                $this->formaIzmenaPodataka($this->session->userdata('user')['idKor']);
+            }else{
+                $idKor = $this->session->userdata('user')['idKor'];
+                $korisnicko= $this->input->post('korisnicko');
+                $email= $this->input->post('email');
+
+                $ime= $this->input->post('ime');
+                $prezime = $this->input->post('prezime');
+                $datum = $this->input->post('datum');
+                $drzavljanstvo = $this->input->post('drzavljanstvo');
+                $telefon = $this->input->post('telefon');
+                $adresa = $this->input->post('adresa');
+                $mesto = $this->input->post('mesto');
+                $status = $this->input->post('status');
+                $kurs = $this->input->post('kurs');
+                
+                $this->UserModel->izmeniKorisnika($idKor, $korisnicko, $email);
+                $this->UserModel->izmeniStudenta($idKor, $ime, $prezime, $datum, $drzavljanstvo, $telefon, $adresa, $mesto, $status, $kurs);
+                
+                redirect('User');
+            }
+             
+            
+        }
+        
+        
+        public function valid_password($password = ''){
+		$password = trim($password);
+		$regex_lowercase = '/[a-z]/';
+		$regex_uppercase = '/[A-Z]/';
+		$regex_number = '/[0-9]/';
+		$regex_special = '/[!@#$%^&*()\-_=+{};:,<.>§~]/';
+		if (empty($password))
+		{
+			$this->form_validation->set_message('valid_password', '{field} je obavezna. ');
+			return FALSE;
+		}
+		if (preg_match_all($regex_lowercase, $password) < 1)
+		{
+			$this->form_validation->set_message('valid_password', '{field} mora da sadrži bar jedno malo slovo.');
+			return FALSE;
+		}
+		if (preg_match_all($regex_uppercase, $password) < 1)
+		{
+			$this->form_validation->set_message('valid_password', '{field} mora da sadrži bar jedno veliko slovo.');
+			return FALSE;
+		}
+		if (preg_match_all($regex_number, $password) < 1)
+		{
+			$this->form_validation->set_message('valid_password', '{field} mora da sadrži bar jednu cifru.');
+			return FALSE;
+		}
+		if (preg_match_all($regex_special, $password) < 1)
+		{
+			$this->form_validation->set_message('valid_password', '{field} mora da sadrži bar jedan specijalni karakter:' . ' ' . htmlentities('!@#$%^&*()\-_=+{};:,<.>§~'));
+			return FALSE;
+		}
+		if (strlen($password) < 5)
+		{
+			$this->form_validation->set_message('valid_password', '{field} mora da bude duža od 4 karaktera.');
+			return FALSE;
+		}
+		if (strlen($password) > 32)
+		{
+			$this->form_validation->set_message('valid_password', '{field} mora da bude kraća od 33 karaktera.');
+			return FALSE;
+		}
+		return TRUE;
+	}
+        
+        public function validateAge($birthday, $age) {
+
+        if (is_string($birthday)) {
+            $birthday = strtotime($birthday); 
+        }
+        if (time() - $birthday < $age * 31536000) {
+            $this->form_validation->set_message('validateAge', 'Morate biti stariji od 18.');
+            return false;
+        }
+        return true;
+    }
+        
+   
 }
---
+++ application/views/middle/izmenaPodataka.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-4">
+            <div class="izmenaSlike">
+                
+                    <?php 
+                    $idKor = $this->session->userdata('user')['idKor'];
+                    $tip = $this->session->userdata('user')['tip'];
+                       if(is_dir('./userImg/'.$idKor)== false or 
+                       empty(array_diff(scandir('./userImg/'.$idKor), array('.', '..')))){
+                    if($tip == "s"){?>
+                        <img src="<?php echo base_url();?>/userImg/basicUser.png" class="img-fluid">
+                    <?php
+                    }else if($tip == "k"){?>
+                        <img src="<?php echo base_url();?>/userImg/basicLogo.png" class="img-fluid">
+                   
+                       <?php 
+                   } 
+                   ?>
+                     <form name="imageForm" method="POST" action="<?php echo site_url('User/novaSlika')?>" enctype="multipart/form-data">
+                          <input type="file" name="image">
+                          <input type="submit" value="Dodaj sliku" name="dodaj">
+                     </form>
+                  
+            <?php
+                   
+            }else{
+                $dir= './userImg/'.$idKor;
+                $allPhotos= scandir($dir);
+                $onlyPhotos = array_diff($allPhotos, array('.', '..'));
+                foreach($onlyPhotos as $onePhoto){ 
+            ?>
+            
+                <img class="img img-fluid" src="<?php echo base_url().'/'.$dir.'/'.$onePhoto?>">       
+                    
+            <?php
+                }
+            }
+            ?>    
+            </div>
+<!--            <input type="button" value="IZmeni fotografiju" class="btn btn-primary btn-lg">-->
+        </div>
+        <div class="col-6">
+        <div class="izmenaPodatakaForma">
+        <?php foreach ($podaci as $podatak){ ?>
+            <form method="POST" action="<?php echo site_url('User/izmeniPodatke')?>" class="formaReg">
+                Korisnicko ime:
+
+                             <input class="form-control" name="korisnicko" type="text" value="<?php echo $this->session->userdata('user')['korisnicko'] ?>"> <?php echo form_error('korisnicko') ?> <br/>
+                             <input type="hidden" name="originalKorisnicko" value="<?php echo $this->session->userdata('user')['korisnicko'] ?>">
+                             Ime:
+                             <input class="form-control" name="ime" type="text" value="<?php echo $podatak['ime'] ?>"><?php echo form_error('ime') ?> <br/>
+                             Prezime:
+                             <input class="form-control" name="prezime" type="text" value="<?php echo $podatak['prezime'] ?>"><?php echo form_error('prezime') ?> <br/>
+                             Kurs koji ste zavrsili:
+                             <select class="form-control" name="kurs">
+                                 <?php 
+                                 foreach ($kursevi as $kurs){
+                                     $naziv = $kurs["naziv"];
+                                     $idK = $kurs["idKurs"];
+                                     if($podatak["kurs"] == $naziv){
+                                         echo "<option value='$idK' selected>$naziv</option>";
+                                     }else{
+                                     echo "<option value='$idK'>$naziv</option>";
+                                     }
+                                 }
+                                 ?>
+                             </select> <br/> <?php echo form_error('kurs');?>   
+                             Drzavljanstvo:
+                             <select class="form-control" name="drzavljanstvo">
+                                 <?php 
+                                 foreach ($drzavljanstvo as $drz){
+                                     $naziv = $drz["naziv"];
+                                     $idD = $drz["idDrz"];
+                                     if($podatak["drzavljanstvo"] == $naziv){
+                                         echo "<option value='$idD' selected>$naziv</option>";
+                                     }else{
+                                     echo "<option value='$idD'>$naziv</option>";
+                                     }
+                                 }
+                                 ?>
+                             </select><br/> <?php echo form_error('drzavljanstvo');?>
+                             Broj telefona:
+                             <input class="form-control" name="telefon" type="text" value="<?php echo $podatak['telefon'] ?>" ><?php echo form_error('telefon') ?> <br/>  
+                             Adresa:
+                             <input class="form-control" name="adresa" type="text" value="<?php echo $podatak['adresa'] ?>" ><?php echo form_error('adresa') ?> <br/>
+                             Mesto stanovanja:
+                             <div id="selectmesto">
+                             <select class="form-control" name="mesto" onchange="dodajInput('mesto')" id="listamesto"> 
+                                   <?php 
+                                 foreach ($mesta as $mesto){
+                                     $naziv = $mesto["naziv"];
+                                     $idG = $mesto["idGra"];
+                                     if($podatak["grad"] == $naziv){
+                                        echo "<option value='$idG' selected>$naziv</option>"; 
+                                     }else{
+                                     echo "<option value='$idG'>$naziv</option>";
+                                     }
+                                 }
+                                 ?>
+                                <option class="dodaj" value="dodaj">Dodaj novo mesto</option>
+                                </select>
+                             </div>
+                             <div id="mesto">
+                             </div> <br/>
+                             <?php echo form_error('mesto');?>
+                             Trenutni status:
+                             <select class="form-control" name="status">
+                                 <option value="student">Student</option>
+                                 <option value="nezaposleni">Nezaposleni</option>
+                                 <option value="zaposleni">Zaposleni</option>   
+                             </select><br/><?php echo form_error('status');?>
+
+                            E-mail:
+                             <input class="form-control" type="email" name="email"  value="<?php echo $this->session->userdata('user')['email'] ?>"><?php echo form_error('email') ?><br/>
+                             <input type="hidden" name="originalEmail" value="<?php echo $this->session->userdata('user')['email'] ?>">
+
+                             Datum rodjenja: 
+                                <input class="form-control" type="date" name="datum"  value="<?php echo $podatak["datum"] ?>"> 
+
+                             <?php echo form_error('datum') ?>  <br/>
+                             <div class="form-row">
+                                 <div class="col-10 offset-1 text-center">
+                             <input type="submit"  name="izmenaPodataka" value="Sacuvaj izmene" class="btn btn-success btn-lg">  
+                             <a href="<?php echo site_url('User')?>" class="btn btn-lg btn-danger">Odustani</a>
+                                 </div>
+                             </div>
+                             </form>
+
+                    <?php } ?>
+            </div>
+        </div>
+   </div>
+</div>
