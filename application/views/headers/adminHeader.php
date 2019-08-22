6ab81651ed6413556ff01f9ae2751351868f506d
gordst <33371225+gordst@users.noreply.github.com>
2019-08-22 17:34:24 +0200

    prava pristupa - diskusije - promene, css admin dodato iz css student

--- application/views/headers/adminHeader.php
+++ application/views/headers/adminHeader.php
@@ -7,6 +7,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+         <script src="https://kit.fontawesome.com/2dd4f4a4ca.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
         <title></title>
     </head>
     <body>
