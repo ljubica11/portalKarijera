0e48cd0ed7d0624a89795c5b13f56c743dbd4092
SanjaKordic <kordic.l.sanja@gmail.com>
2019-08-24 12:17:48 +0200

    sredjeni linkovi u footeru
    Sredjeno za korisnike i admina

--- application/views/footers/userFooter.php
+++ application/views/footers/userFooter.php
@@ -1,9 +1,9 @@
 ﻿
 
         <div class="<?php if($tip == "student") echo "sfooter"; else if ($tip == "kompanija") echo "kfooter"; else if ($tip == "admin") echo "afooter"?>">
-            <a href="<?php echo base_url() ?>/index.php/Login/oNama">O nama</a>
-            <a href="<?php echo base_url() ?>/index.php/Login/oNama#target">Kontakt</a>
-            <a href="<?php echo base_url() ?>/index.php/Login/usloviKoriscenja">Uslovi korišćenja</a>
+            <a href="<?php echo base_url() ?>/index.php/User/oNama">O nama</a>
+            <a href="<?php echo base_url() ?>/index.php/User/oNama#target">Kontakt</a>
+            <a href="<?php echo base_url() ?>/index.php/User/usloviKoriscenja">Uslovi korišćenja</a>
 
             <span class="copy"> copyright Karijera.rs </span>
         </div>
