69f090fd651ca3bc90c17d2fb7a27f4a1c90b650
gordst <33371225+gordst@users.noreply.github.com>
2019-12-05 15:15:34 +0100

    hashout

--- application/models/RegistrationModel.php
+++ application/models/RegistrationModel.php
@@ -1,11 +1,12 @@
 <?php
 
-class RegistrationModel extends CI_Model{
-    
-    public function dodajKorisnika($korisnicko, $lozinka, $email, $vidEmail, $tip, $cekaOdobrenje){
+class RegistrationModel extends CI_Model
+{
+    public function dodajKorisnika($korisnicko, $lozinka, $email, $vidEmail, $tip, $cekaOdobrenje)
+    {
         $data = [
             "korisnicko" => $korisnicko,
-            "lozinka" => password_hash($lozinka, PASSWORD_DEFAULT),
+            "lozinka" => $lozinka,
             "email" => $email,
             "vidljivostEmail" => $vidEmail,
             "tip" => $tip,
@@ -13,10 +14,11 @@ class RegistrationModel extends CI_Model{
         ];
         
         
-        $this->db->insert ( "korisnik", $data );
+        $this->db->insert("korisnik", $data);
     }
     
-    public function dohvatiId($korisnicko){
+    public function dohvatiId($korisnicko)
+    {
         $this->db->select('idKor');
         $this->db->where('korisnicko', $korisnicko);
         $this->db->from('korisnik');
@@ -24,7 +26,8 @@ class RegistrationModel extends CI_Model{
         return $query->result_array();
     }
     
-    public function dodajStudenta($ime, $srednjeIme, $prezime, $datum, $pol, $drzavljanstvo, $telefon, $adresa, $mesto, $pin, $status, $kurs, $idKor, $vidAdresa, $vidTel){
+    public function dodajStudenta($ime, $srednjeIme, $prezime, $datum, $pol, $drzavljanstvo, $telefon, $adresa, $mesto, $pin, $status, $kurs, $idKor, $vidAdresa, $vidTel)
+    {
         $data = [
             "ime" => $ime,
             "srednjeIme" => $srednjeIme,
@@ -44,10 +47,10 @@ class RegistrationModel extends CI_Model{
         ];
         
         $this->db->insert("student", $data);
-        
     }
     
-    public function dodajKompaniju($naziv, $sediste, $pib, $telefoni, $opis, $oblast, $brojzap, $sajt, $idKor){
+    public function dodajKompaniju($naziv, $sediste, $pib, $telefoni, $opis, $oblast, $brojzap, $sajt, $idKor)
+    {
         $data = [
             "naziv" => $naziv,
             "sediste" => $sediste,
@@ -57,38 +60,41 @@ class RegistrationModel extends CI_Model{
             "oblastDelovanja" => $oblast,
             "brojZap" => $brojzap,
             "sajt" => $sajt,
-            "idKor" => $idKor  
+            "idKor" => $idKor
         ];
         
         $this->db->insert("kompanija", $data);
     }
     
-    public function dodajInteresovanjaZaKorisnika($idKor, $jednoInteresovanje){
-       
+    public function dodajInteresovanjaZaKorisnika($idKor, $jednoInteresovanje)
+    {
         $data = ["idKor" => $idKor, "idInt" => $jednoInteresovanje];
         $this->db->insert("imainteresovanja", $data);
     }
     
     
-    public function dodajVestineZaKorisnika($idKor, $jednaVestina){
+    public function dodajVestineZaKorisnika($idKor, $jednaVestina)
+    {
         $data = ["idKor" => $idKor, "idVes" => $jednaVestina];
-        $this->db->insert("imavestine", $data); 
+        $this->db->insert("imavestine", $data);
     }
     
-    public function dodajStudije($idKor, $univerzitet, $fakultet, $sedisteFak, $nivo, $godinaStu){
+    public function dodajStudije($idKor, $univerzitet, $fakultet, $sedisteFak, $nivo, $godinaStu)
+    {
         $data = [
             "idKor" => $idKor,
             "univerzitet" => $univerzitet,
             "sediste" => $sedisteFak,
             "nivo" => $nivo,
             "godinaStudija" => $godinaStu,
-            "idFak" => $fakultet   
+            "idFak" => $fakultet
         ];
         
         $this->db->insert("studije", $data);
     }
     
-    public function dodajDiplomuZaKorisnika($idKor, $idFak, $odsek, $zvanje, $nivo, $godUpisa, $godZavrsetka, $vidljivost){
+    public function dodajDiplomuZaKorisnika($idKor, $idFak, $odsek, $zvanje, $nivo, $godUpisa, $godZavrsetka, $vidljivost)
+    {
         $data= [
             "idKor" => $idKor,
             "odsek" => $odsek,
@@ -103,7 +109,8 @@ class RegistrationModel extends CI_Model{
         $this->db->insert("diploma", $data);
     }
     
-    public function dodajIskustvoZaKorisnika($idKor, $kompanija, $mesto, $pozicija, $od, $do, $vidljivost){
+    public function dodajIskustvoZaKorisnika($idKor, $kompanija, $mesto, $pozicija, $od, $do, $vidljivost)
+    {
         $data =[
             "idKor" => $idKor,
             "kompanija" => $kompanija,
@@ -116,5 +123,4 @@ class RegistrationModel extends CI_Model{
         
         $this->db->insert("zaposlenje", $data);
     }
-
 }
--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -1,24 +1,27 @@
 <?php
 
 
-class UserModel extends CI_Model{
-    
-    public function __construct() {
+class UserModel extends CI_Model
+{
+    public function __construct()
+    {
         parent::__construct();
         
         $this->load->database();
     }
     
-     public function login($username, $pass){
+    public function login($username, $pass)
+    {
         $this->db->where('korisnicko', $username);
         $this->db->where('lozinka', $pass);
         $this->db->where('cekaOdobrenje', null);
         $this->db->from('korisnik');
         $query=$this->db->get();
-        return $query->result_array ();
+        return $query->result_array();
     }
     
-    public function podaciZaStudenta($id){
+    public function podaciZaStudenta($id)
+    {
         $this->db->select('ime, prezime, datum, telefon, vidljivostTelefon, adresa, vidljivostAdresa, status, sifgradovi.naziv as grad, sifdrzavljanstvo.naziv as drzavljanstvo, sifkurs.naziv as kurs');
         $this->db->from('student');
         $this->db->join('sifgradovi', 'student.mesto = sifgradovi.idGra');
@@ -26,38 +29,42 @@ class UserModel extends CI_Model{
         $this->db->join('sifkurs', 'student.idKurs = sifkurs.idKurs');
         $this->db->where('student.idKor', $id);
         $query=$this->db->get();
-        return $query->result_array ();
+        return $query->result_array();
     }
     
-    public function imaInteresovanja($id){
+    public function imaInteresovanja($id)
+    {
         $this->db->select('naziv');
         $this->db->from('imainteresovanja');
         $this->db->join('sifinteresovanja', 'imainteresovanja.idInt = sifinteresovanja.idInt');
         $this->db->where('idKor', $id);
         $query=$this->db->get();
-        return $query->result_array ();
+        return $query->result_array();
     }
     
-    public function imaVestine($id){
+    public function imaVestine($id)
+    {
         $this->db->select('naziv');
         $this->db->from('imavestine');
         $this->db->join('sifvestine', 'imavestine.idVes = sifvestine.idVes');
         $this->db->where('idKor', $id);
         $query=$this->db->get();
-        return $query->result_array ();
+        return $query->result_array();
     }
     
-    public function imaDiplomu($id){
+    public function imaDiplomu($id)
+    {
         $this->db->select('odsek, godinaUpisa, godinaZavrsetka, zvanje, naziv, nivo');
         $this->db->from('diploma');
         $this->db->join('siffakulteti', 'diploma.idFak = siffakulteti.idFak');
         $this->db->where('idKor', $id);
         $this->db->where('diploma.vidljivost', null);
         $query=$this->db->get();
-        return $query->result_array ();
+        return $query->result_array();
     }
     
-    public function radnoIskustvo($id){
+    public function radnoIskustvo($id)
+    {
         $this->db->select('sifkompanija.naziv as kompanija, sifgradovi.naziv as grad, sifpozicija.naziv as pozicija, od, do');
         $this->db->from('zaposlenje');
         $this->db->join('sifkompanija', 'zaposlenje.kompanija = sifkompanija.idSifKo');
@@ -66,11 +73,11 @@ class UserModel extends CI_Model{
         $this->db->where('idKor', $id);
         $this->db->where('zaposlenje.vidljivost', null);
         $query=$this->db->get();
-        return $query->result_array ();
-        
+        return $query->result_array();
     }
     
-    public function trenutnoStudira($id){
+    public function trenutnoStudira($id)
+    {
         $this->db->select('sifuniverziteti.naziv as univerzitet, siffakulteti.naziv as fakultet, sifgradovi.naziv as grad, nivo, godinaStudija');
         $this->db->from('studije');
         $this->db->join('sifuniverziteti', 'studije.univerzitet = sifuniverziteti.idUni');
@@ -78,31 +85,35 @@ class UserModel extends CI_Model{
         $this->db->join('sifgradovi', 'studije.sediste = sifgradovi.idGra');
         $this->db->where('idKor', $id);
         $query=$this->db->get();
-        return $query->result_array ();
+        return $query->result_array();
     }
     
-    public function podaciZaKompaniju($id){
+    public function podaciZaKompaniju($id)
+    {
         $this->db->select('kompanija.naziv as naziv, pib, telefoni, opis, oblastDelovanja, brojZap, sajt, sifgradovi.naziv as sediste');
         $this->db->from('kompanija');
         $this->db->join('sifgradovi', 'kompanija.sediste = sifgradovi.idGra');
         $this->db->where('idKor', $id);
         $query=$this->db->get();
-        return $query->result_array ();
+        return $query->result_array();
     }
       
-    public function imaObavestenja($id){
+    public function imaObavestenja($id)
+    {
         $query = $this->db->get_where('obavestenja', array('autor' => $id));
-        return $query->result_array ();
+        return $query->result_array();
     }
     
-    public function izmeniKorisnika($idKor, $korisnicko, $email){
+    public function izmeniKorisnika($idKor, $korisnicko, $email)
+    {
         $this->db->set('korisnicko', $korisnicko);
         $this->db->set('email', $email);
         $this->db->where('idKor', $idKor);
         $this->db->update('korisnik');
     }
     
-    public function izmeniStudenta($idKor, $ime, $prezime, $datum, $drzavljanstvo, $telefon, $adresa, $mesto, $status, $kurs){
+    public function izmeniStudenta($idKor, $ime, $prezime, $datum, $drzavljanstvo, $telefon, $adresa, $mesto, $status, $kurs)
+    {
         $data = array(
             'ime' => $ime,
             'prezime' => $prezime,
@@ -114,23 +125,7 @@ class UserModel extends CI_Model{
             'status' => $status,
             'idKurs' => $kurs);
 
-            $this->db->where('idKor', $idKor);
-            $this->db->update('student', $data);
+        $this->db->where('idKor', $idKor);
+        $this->db->update('student', $data);
     }
-    
-    
-    /**
-     * pass hash
-     * @param type $username
-     * @param type $pass
-     */
-    public function hashstored($username, $pass){
-        
-     $data = ['lozinka' => password_hash($pass, PASSWORD_DEFAULT)];
-     $this->db->where('korisnicko', $username);
-     $this->db->update('korisnik', $data);
-     
-             
-    } 
-      
 }
