d30c808af3bda2adb80b406254f9e178b2ac039b
ljubica11 <ljkrstic1@gmail.com>
2019-12-05 19:19:08 +0100

    Responsive navbar

--- application/views/headers/guestHeader.php
+++ application/views/headers/guestHeader.php
@@ -29,6 +29,9 @@
         id="brand-image"
         src="<?php echo base_url()?>//images/logo.png" width="90"
         height="60" title="Logo" alt="Logo">PortalKarijera</a>
+     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
