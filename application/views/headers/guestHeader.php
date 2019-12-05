f71900d7b56431a94922bdbb2639a1cce06d7701
ljubica11 <ljkrstic1@gmail.com>
2019-12-05 16:33:09 +0100

    Izmena linka za css (proba)

--- application/views/headers/guestHeader.php
+++ application/views/headers/guestHeader.php
@@ -2,7 +2,7 @@
 
 <head>
   <meta charset="UTF-8">
-  <link rel="stylesheet" type="text/css" href="http://localhost/portalKarijera/css/styleGuest.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/css/styleGuest.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
